eaacda03bc304c30c43c1e51175f734424c7fd2f
Torr7s <torresmiranda.rj@gmail.com>
2022-02-25 23:33:04 -0300

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerConfigFile from './swagger.config.json'
 
 import { userRouter } from './routes/users.routes';
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerConfigFile))
 
 app.use('/users', userRouter)
 
